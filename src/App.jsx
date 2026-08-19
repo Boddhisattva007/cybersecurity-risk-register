5f1fed3cfed1b36f6c232be15d2f3413b3bd9018
Brian Taylor <brtaylor0010@gmail.com>
2026-08-18 19:41:51 -0500

    Initialized React app with Vite and React Router

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    <div>
+      <h1>Cybersecurity Risk Register</h1>
+    </div>
+
+    </>
+  )
+}
+
+export default App
