10c2046c58bf8f524e66a9bc263568527cfe41a9
Brian Taylor <brtaylor0010@gmail.com>
2026-08-31 20:25:49 -0500

    completed Home and About pages

--- src/Main View Pages/About/About.jsx
+++ src/Main View Pages/About/About.jsx
@@ -3,9 +3,31 @@
 function About() {
 
     return (
-        <h1>
-            About Page
-        </h1>
+
+        <div>
+            <h1>About Page</h1>
+
+            <h2>Risk Assesment</h2>
+
+            <p>Likelihood x Impact = Risk Score = Severity</p>
+
+            <p>
+                Each risk is assigned a likelihood and impact by the user. These values
+                are used to calculate a risk score and determine the overall risk severity.
+            </p>
+
+            <h2>Application Features</h2>
+
+            <ul>
+                <li>Add Risks</li>
+                <li>Edit Risks</li>
+                <li>Delete Risks</li>
+                <li>Calculate Risk Score</li>
+                <li>Filter Risks</li>
+
+            </ul>
+
+        </div>
     );
 }
 
--- src/Main View Pages/Home/Home.jsx
+++ src/Main View Pages/Home/Home.jsx
@@ -6,6 +6,17 @@ function Home() {
 
             <h1>Home Page</h1>
 
+            <p>A simple way to identify, assess, and manage cybersecurity risks.</p>
+
+            <p>How It Works</p>
+
+            <ol>
+                <li>Record a Risk</li>
+                <li>Assign Likelihood</li>
+                <li>Assign Impact</li>
+                <li>Review Calculated Severity</li>
+            </ol>
+
         </section>
     );
 }
